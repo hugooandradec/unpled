31f19ad1863ec37891f85e882414be8122ea2cb4
Victor Hugo <hugooandradec@gmail.com>
2026-01-04 21:01:12 -0300

    auto commit

--- sw.js
+++ sw.js
@@ -1,24 +1,39 @@
-const CACHE_NAME = "unpled-v1";
+const CACHE_NAME = "unpled-v2";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./css/styles.css",
+
+  // CSS corretos (os que seu index.html realmente usa)
+  "./css/global.css",
+  "./css/index.css",
+  "./css/collection.css",
+
+  // JS
   "./js/app.js",
   "./js/ui.js",
   "./js/gacha.js",
   "./js/storage.js",
+  "./js/collection.js",
+
+  // Dados
   "./data/cards.js",
-  "./manifest.webmanifest"
+
+  // PWA
+  "./manifest.webmanifest",
+
+  // Ícones (ajusta conforme seus arquivos reais)
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
@@ -26,7 +41,7 @@ self.addEventListener("activate", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request))
   );
